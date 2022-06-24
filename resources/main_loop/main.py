3befc7982511b09bdf30f1612635dcb40b9061d4
Andrey <androbaza@tuta.io>
2022-06-24 16:21:05 +0200

    merge video recording func to main for easier testing

--- resources/main_loop/main.py
+++ resources/main_loop/main.py
@@ -1,5 +1,54 @@
-import RGBThread, make_rgb_view
+# import RGBThread, make_rgb_view
+import cv2, os
 import RPi.GPIO as GPIO
+
+def gstreamer_pipeline(
+    camera_id=0,
+    capture_width=3264,
+    capture_height=2464,
+    out_width=1024,
+    out_height=768,
+    framerate=10,
+    flip_method=0,
+):
+
+    gst_string = f"""
+    nvarguscamerasrc sensor_id={camera_id} ! 
+    video/x-raw(memory:NVMM),
+    width=(int){capture_width}, height=(int){capture_height},
+    format=(string)NV12, framerate=(fraction){framerate}/1 !
+    nvvidconv flip-method={flip_method} !
+    video/x-raw, width=(int){out_width}, height=(int){out_height}, format=(string)BGRx !
+    videoconvert !
+    video/x-raw, format=(string)BGR !
+    appsink max-buffers=1 drop=true
+    """
+
+    return gst_string
+
+
+
+def record_video(camera_id, duration):
+
+    gst_string = gstreamer_pipeline(
+        camera_id=camera_id,
+        framerate=30,
+        flip_method=2,
+    )
+
+    capture = cv2.VideoCapture(gst_string, cv2.CAP_GSTREAMER)
+    video_save = cv2.VideoWriter('videos/pulse5s.mp4', cv2.VideoWriter_fourcc(*'mp4v'), gstreamer_pipeline.framerate,(gstreamer_pipeline.out_width, gstreamer_pipeline.out_height))
+
+    for frame_n in range(0, duration * gstreamer_pipeline.framerate):
+        ret, frame = capture.read()
+        cv2.imshow(str(frame_n), frame)
+        video_save.write(frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    capture.release()
+    video_save.release()
+    cv2.destroyAllWindows()
+
 
 def exit_handler():
     print("exit handler called")
@@ -27,7 +76,6 @@ def setup_display(display_addr):
 HZ_CAP = 20
 LOG_DIR = "logs"
 VIS_WIN_NAME = "RGB view"
-IR_WIN_NAME = "IR view"
 
 VIS_BBOX_COLOR = (0, 0, 255)  # red
 IR_BBOX_COLOR = (0, 255, 0)  # green
@@ -62,41 +110,39 @@ CMAP_TEMP_MAX = 40
 
 def mainloop():
 
-
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(15, GPIO.IN)
 	button_state_old = 0
 	while():
-		
+		# value is 1 when the butoon is idle
 		button_state_upd = GPIO.input(15)
+
 		# value is 0 when the button is pressed
 		# enter the video recording/inference state when the button is pressed and exit the loop
 		if (button_state_upd == 0 and button_state_old == 1) 
-            rgb_view = make_rgb_view(rgb_arr, scores, boxes, landms, VIS_WIN_SIZE)
-            if SHOW_DISPLAY:
-                cv2.imshow(VIS_WIN_NAME, rgb_view)
-                # cv2.imshow(IR_WIN_NAME, ir_view)
-                key = cv2.waitKey(1) & 0xFF
+            # rgb_view = make_rgb_view(VIS_WIN_SIZE)
+            # if SHOW_DISPLAY:
+            #     cv2.imshow(VIS_WIN_NAME, rgb_view)
+            #     # cv2.imshow(IR_WIN_NAME, ir_view)
+            #     key = cv2.waitKey(1) & 0xFF
 
-                # if the `q` key was pressed in the cv2 window, we break from the loop and exit the program
-                if key == ord("q"):
-                    break            
+            #     # if the `q` key was pressed in the cv2 window, we break from the loop and exit the program
+            #     if key == ord("q"):
+            #         break          
+
+
 			# do the video recording
-
+            record_video(camera_id, 5)
 
 
             # feed the video to pyVHR
 
 
             # delete the video from disk
-
+            os.remove('videos/pulse5s.mp4')
 
             button_state_old = 0
-            continue
-
-
-
-
+            
 		button_state_old = button_state_upd
 	GPIO.cleanup()
 
